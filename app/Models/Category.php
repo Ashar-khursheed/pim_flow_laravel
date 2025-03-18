54a4d40dcbd1dc806dd5e48ed65f67fcf5f5b0a4
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-03-18 13:43:43 +0530

    commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,43 +7,54 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
-    protected $table = 'ec_product_categories';
+	protected $table = 'ec_product_categories';
 
-    protected $fillable = [
-        'name', 'parent_id', 'description', 'status', 'order',
-        'image', 'is_featured', 'icon', 'icon_image', 'slug'
-    ];
+	protected $fillable = [
+		'name', 'parent_id', 'description', 'status', 'order',
+		'image', 'is_featured', 'icon', 'icon_image', 'slug'
+	];
 
-    public function parent()
-    {
-        return $this->belongsTo(Category::class, 'parent_id');
-    }
+	public function parent()
+	{
+		return $this->belongsTo(Category::class, 'parent_id');
+	}
 
-    public function scopeLastChildCategories($query, $parentId)
-    {
-        return $query->where('parent_id', '!=', 0)
-            ->whereNotIn('id', function ($subQuery) {
-                $subQuery->select('parent_id')
-                    ->from('ec_product_categories')
-                    ->whereNotNull('parent_id');
-            })
-            ->whereHas('parent', function ($parentQuery) use ($parentId) {
-                $parentQuery->where('parent_id', $parentId);
-            });
-    }
+	public function scopeLastChildCategories($query, $parentId)
+	{
+		return $query->where('parent_id', '!=', 0)
+		->whereNotIn('id', function ($subQuery) {
+			$subQuery->select('parent_id')
+			->from('ec_product_categories')
+			->whereNotNull('parent_id');
+		})
+		->whereHas('parent', function ($parentQuery) use ($parentId) {
+			$parentQuery->where('parent_id', $parentId);
+		});
+	}
 
 	public function children()
 	{
 		return $this->hasMany(Category::class, 'parent_id');
 	}
 
-    public function attributes()
-    {
-        return $this->morphedByMany(Attribute::class, 'relational', 'attribute_group_categories', 'category_id', 'relational_id');
-    }
+	public function attributes()
+	{
+		return $this->morphedByMany(Attribute::class, 'relational', 'attribute_group_categories', 'category_id', 'relational_id');
+	}
 
-    public function attributeGroups()
-    {
-        return $this->morphedByMany(AttributeGroup::class, 'relational', 'attribute_group_categories', 'category_id', 'relational_id');
-    }
+	public function attributeGroups()
+	{
+		return $this->morphedByMany(AttributeGroup::class, 'relational', 'attribute_group_categories', 'category_id', 'relational_id');
+	}
+
+	public static function getLeafCategories($category)
+	{
+		if ($category->children->isEmpty()) {
+			return collect([$category]);
+		}
+
+		return $category->children->flatMap(function ($child) {
+			return self::getLeafCategories($child);
+		});
+	}
 }
