55846e78ba0d47c3e49f28666f8089f4d7d9af75
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-03-24 10:34:34 +0530

    Update CategoryAttributeController.php

--- app/Http/Controllers/CategoryAttributeController.php
+++ app/Http/Controllers/CategoryAttributeController.php
@@ -53,13 +53,18 @@ class CategoryAttributeController extends BaseController
 			'attributeGroups.groupAttributes:id,code,name'
 		])->whereDoesntHave('children');
 
-		if($request->filled('page') && $request->filled('length')){
-			$page = $request->input('page');
-			$length = $request->input('length');
-			$records = $records->offset(($page - 1)*$length)->limit($length);
-		}
+		/* Pagination */
+		if ($request->filled('page') && $request->filled('length')) {
+			$page = (int) $request->input('page');
+			$length = (int) $request->input('length');
+			$totalRecords = $records->count();
+			$totalPages = ceil($totalRecords / $length);
 
-		$records = $records->get(['id', 'name', 'parent_id']);
+			$records = $records->offset(($page - 1) * $length)->limit($length)->get(['id', 'name', 'parent_id']);
+		} else {
+			$records = $records->get(['id', 'name', 'parent_id']);
+			$totalRecords = $records->count();
+		}
 
 		// Hide pivot data manually
 		$records->each(function ($category) {
@@ -73,7 +78,9 @@ class CategoryAttributeController extends BaseController
 		return response()->json([
 			'success' => true,
 			'message' => 'Categrory List with Atribute & Attribute Group',
-			'data' => $records
+			'data' => $records,
+			'total_pages' => $totalPages ?? 1,
+			'total_records' => $totalRecords,
 		]);
 	}
 
