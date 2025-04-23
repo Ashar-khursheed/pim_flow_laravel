61dea4573f9a421c25e348775b550c7aed371f34
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-04-23 12:44:15 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use OpenApi\Annotations as OA;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 
 class UserController extends BaseController
