dff1262fcce5996073176a760b3025868cdbe333
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-05-01 18:04:14 +0530

    commit

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,7 @@ use App\Http\Controllers\ProductImageUploadController;
 use App\Http\Controllers\DocumentUploadController;
 use App\Http\Controllers\SupplierScoreController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\VendorDocumentController;
 use App\Http\Controllers\DashboardController;
 
 
@@ -92,6 +93,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
 	Route::apiResource('brand-temp-2', BrandTemp2Controller::class);
 	Route::apiResource('brand-temp-3', BrandTemp3Controller::class);
 
+	Route::post('/vendors/{vendor_id}/documents', [VendorDocumentController::class, 'store']);
+	Route::get('/vendors/{vendor_id}/documents', [VendorDocumentController::class, 'show']);
 	Route::post('/vendors/import', [VendorController::class, 'import']);
 	Route::apiResource('vendors', VendorController::class);
 	Route::apiResource('pre-onboarding-vendors', PreOnboardingVendorController::class);
