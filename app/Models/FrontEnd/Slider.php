298dd4e9ba9aff209bef9db4e1eb0115bc9a24c5
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-14 16:45:47 +0500

    cgasd

--- app/Models/FrontEnd/Slider.php
+++ app/Models/FrontEnd/Slider.php
@@ -3,24 +3,7 @@ namespace App\Models\FrontEnd;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-/**
- * @OA\Schema(
- *     schema="SimpleSlider",
- *     type="object",
- *     title="SimpleSlider",
- *     required={"id", "name", "key", "status"},
- *     @OA\Property(property="id", type="integer", example=1),
- *     @OA\Property(property="name", type="string", example="Homepage Banner"),
- *     @OA\Property(property="key", type="string", example="home_slider"),
- *     @OA\Property(property="description", type="string", example="Main homepage slider"),
- *     @OA\Property(property="status", type="string", example="active"),
- *     @OA\Property(
- *         property="sliderItems",
- *         type="array",
- *         @OA\Items(type="object") // Replace with a ref if you define a separate SliderItem schema
- *     )
- * )
- */
+
 
 class Slider extends Model
 {
