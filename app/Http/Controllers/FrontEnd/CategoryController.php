bb32ae451610dd22f8c0bfd5ee48b467051c0708
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-19 20:52:09 +0500

    sds

--- app/Http/Controllers/FrontEnd/CategoryController.php
+++ app/Http/Controllers/FrontEnd/CategoryController.php
@@ -2069,7 +2069,7 @@ use Illuminate\Support\Facades\Auth;
                 return [
                     'id' => $brand->id,
                     'name' => $brand->name,
-                    //  'product_count' => $productCount,
+                     'product_count' => $productCount,
                     'display_name' => $brand->name . ' (' . $productCount . ')'
                 ];
             })->filter(function($brand) {
