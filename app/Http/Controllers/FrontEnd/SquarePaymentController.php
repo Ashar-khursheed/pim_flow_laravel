624df8d9f8ce4cc80986c22d3b0cb716cff16ea9
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-20 22:08:45 +0500

    cg

--- app/Http/Controllers/FrontEnd/SquarePaymentController.php
+++ app/Http/Controllers/FrontEnd/SquarePaymentController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers\FrontEnd;
 
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Square\SquareClient;
 use Square\Models\CreatePaymentRequest;
