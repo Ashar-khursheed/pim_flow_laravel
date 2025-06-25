e570a39d01096cc3ddf283f3f23b23b4bd580e66
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-25 15:11:31 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -330,7 +330,7 @@ Route::middleware(['auth:back-end-api', 'user.guard'])->group(function () {
 
 
 Route::post('frontend/login', [F_AuthController::class, 'store'])->name('f_login');
-Route::post('/frontend/auth/google', [F_AuthController::class, 'login']);
+Route::post('frontend/auth/google', [F_AuthController::class, 'login']);
 Route::post('frontend/register', [F_CustomerController::class, 'register']);
 Route::post('/auth/forgot-password', [AuthController::class, 'sendResetLinkEmail']);
 Route::post('/auth/reset-password', [AuthController::class, 'resetPassword']);
