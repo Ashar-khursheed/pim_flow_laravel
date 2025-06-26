4a656de3ab0295e62be358b2f59d5b2a1ca1e930
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-26 20:15:52 +0500

    sd

---
+++ database/migrations/2025_06_26_151515_drop_assign_to_column_from_product_errors_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('product_errors', function (Blueprint $table) {
+            $table->dropColumn('assign_to');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('product_errors', function (Blueprint $table) {
+            $table->unsignedBigInteger('assign_to')->nullable(); // or use the original type
+        });
+    }
+};
