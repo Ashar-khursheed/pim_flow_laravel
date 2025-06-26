0ff50a95097579a54de1e706179f7380b009122a
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-06-26 13:34:34 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -418,7 +418,7 @@ Route::middleware(['auth:front-end-api', 'customer.guard'])->group(function () {
 
 
 	Route::prefix('/frontend/blogs')->group(function () {
-		Route::put('/{id}/comment', [F_BlogController::class, 'postComment']);
+		Route::post('/{id}/comment', [F_BlogController::class, 'postComment']);
 	});
 
 });
