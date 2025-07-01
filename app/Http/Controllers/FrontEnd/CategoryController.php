3f6c443c21960602da6523bae6a6304580e71969
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-07-01 14:35:00 +0500

    csd

--- app/Http/Controllers/FrontEnd/CategoryController.php
+++ app/Http/Controllers/FrontEnd/CategoryController.php
@@ -2635,7 +2635,7 @@ use Illuminate\Support\Facades\Auth;
               }
 
               $product->per_unit_price = $perUnitPrice;
-              $firstSupplier = $product->product->first();
+              $firstSupplier = $product->productSuppliers->first();
 
             return [
                 'id' => $product->id,
