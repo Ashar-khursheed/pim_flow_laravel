77114908d0a2ed5aaeca0e75c2aaffcf0f839db8
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-07-02 11:49:49 +0530

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -155,7 +155,7 @@ class Product extends Model
 
 	public function tags()
 	{
-		return $this->belongsToMany(Tag::class, 'ec_product_tag_product', 'product_id', 'tag_id');
+		return $this->belongsToMany(Tag::class, 'product_tags', 'product_id', 'tag_id');
 	}
 
 	public function seoMetaData()
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,12 +8,12 @@ class Tag extends Model
 {
     use HasFactory;
 
-    protected $table = 'ec_product_tags'; // Define the correct table name
+    protected $table = 'tags'; // Define the correct table name
 
     protected $fillable = ['name'];
 
     public function products()
     {
-        return $this->belongsToMany(Product::class, 'ec_product_tag_product', 'tag_id', 'product_id');
+        return $this->belongsToMany(Product::class, 'product_tags', 'tag_id', 'product_id');
     }
 }
