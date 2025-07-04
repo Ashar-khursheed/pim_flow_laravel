b86ae3d26ba7683e5cefa5db7edc2590e7123399
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-07-04 16:06:20 +0500

    sd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -180,7 +180,6 @@ class ProductController extends BaseController
 				return [
 					'product_id' => $product->id,
 					'name' => $product->name,
-					'sale_price' => $firstSupplier->sale_price,
 					'margin' => null,
 					'margin_percent' => null,
 					// add other product fields as needed
