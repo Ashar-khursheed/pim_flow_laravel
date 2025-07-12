0a4fe3e737621429f4cd804bd23ff8e685290c68
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-07-12 16:56:14 +0500

    sd

--- app/Http/Controllers/FrontEnd/WishlistController.php
+++ app/Http/Controllers/FrontEnd/WishlistController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\FrontEnd\Wishlist;
 use App\Models\Product;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 class WishlistController extends Controller
 {
     /**
