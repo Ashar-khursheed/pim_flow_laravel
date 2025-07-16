a6921bed1db2943bbdff4c5fd494241009db0007
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-07-16 14:34:13 +0500

    sd

--- database/migrations/2025_07_15_162910_add_business_fields_to_customers_table.php
+++ database/migrations/2025_07_15_162910_add_business_fields_to_customers_table.php
@@ -9,7 +9,7 @@ class AddBusinessFieldsToCustomersTable extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->string('business_name')->after('name');
+            $table->string('business_name')->nullable()->after('name');
             $table->string('business_licence')->nullable()->after('business_name');
             $table->string('trn_number')->nullable()->after('business_licence');
             $table->string('vat_certificate')->nullable()->after('trn_number');
