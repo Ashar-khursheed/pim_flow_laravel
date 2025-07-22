132d1b8fa8f4dfe97d74c9e9cd974eb082d0d567
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-07-22 18:29:04 +0530

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -399,8 +399,7 @@ class ProductController extends BaseController
 			'categories.parent:id,name,parent_id',
 			'categories.parent.parent:id,name,parent_id',
 			'categories.children:id,name,parent_id',
-			'vendors',
-			'productAttributes'
+			'vendors'
 		]);
 
 		$product = Product::with($with)->where('id', $productId)->first(array_merge(['id'], $attributes));
