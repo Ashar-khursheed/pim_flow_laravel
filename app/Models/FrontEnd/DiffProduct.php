9b4728080a5ede5274cc626ce4fc68ae7f8bb6ce
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-07-24 16:31:07 +0500

    sd

---
+++ app/Models/FrontEnd/DiffProduct.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models\FrontEnd;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DiffProduct extends Model
+{
+    protected $table = 'fbt';
+
+    protected $fillable = [
+        'product_id',
+        'dif_id',
+        'priority',
+        'similarity',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function fbt(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'dif_id');
+    }
+}
