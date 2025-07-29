ac3e1514ab21555bb571f30b9c58de1a9cd586ca
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-07-29 20:25:14 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -645,4 +645,4 @@ Route::get('/frontend/google-reviews', [F_GoogleReviewController::class, 'getRev
 
 // Create banner
 Route::get('/frontend/menu-banners', [F_MenuBannerController::class, 'index']);
-Route::get('/frontend/menu-banners/category/{category_id}', [F_MenuBannerController::class, 'getByCategory']);
+Route::get('/frontend/menu-banners/category/{category_id}', [F_MenuBannerController::class, 'show']);
