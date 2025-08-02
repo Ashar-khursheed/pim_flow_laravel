11f423d46c700962e4f97a2f1b9071d9a3103f5f
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-08-02 22:26:46 +0530

    commit

--- app/Http/Controllers/FrontEnd/GlitchErrorController.php
+++ app/Http/Controllers/FrontEnd/GlitchErrorController.php
@@ -119,7 +119,7 @@ class GlitchErrorController extends BaseController
 
 		})->name('Glitch Errors')->dispatch();
 
-		$batch->options['queue'] = 'MAIL';
+		$batch->options['queue'] = 'GLITCH_MAIL';
 		$batch->add(new SendGlitchErrorReportMailJob([
 			'recordId' => $record->id
 		]));
---
+++ app/Jobs/Order/OrderPlacedMailJob.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Jobs\Order;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Bus\Batchable;
+
+use Illuminate\Support\Facades\Mail;
+use App\Models\FrontEnd\Order;
+use App\Mail\Order\OrderPlacedMail;
+
+class OrderPlacedMailJob implements ShouldQueue
+{
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Batchable;
+	public $timeout = 600;
+	public $orderId;
+
+	public function __construct($data)
+	{
+		$this->orderId = $data['recordId'];
+	}
+
+	public function handle(): void
+	{
+		$order = Order::find($this->orderId);
+
+		if (!$order) {
+			$this->fail(new \Exception("Order {$this->orderId} not found"));
+			return;
+		}
+
+		if (!empty($order)) {
+			$to = $order->customer->email;
+
+			Mail::to($to)->send(new OrderPlacedMail($order));
+		}
+	}
+
+	public function failed(\Throwable $exception): void
+	{
+		$jobName = class_basename($this);
+
+		$errorDetails = [
+			'job' => $jobName,
+			'message' => $exception->getMessage(),
+			'file' => $exception->getFile(),
+			'line' => $exception->getLine(),
+			'trace' => $exception->getTraceAsString(),
+		];
+
+		logger()->error("{$jobName} failed", $errorDetails);
+	}
+}
