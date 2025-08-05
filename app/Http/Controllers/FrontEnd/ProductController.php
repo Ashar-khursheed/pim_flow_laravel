4fafcfa516edd47729de2bbc872ad55dbb092037
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-05 21:33:41 +0500

    sd

--- app/Http/Controllers/FrontEnd/ProductController.php
+++ app/Http/Controllers/FrontEnd/ProductController.php
@@ -256,66 +256,110 @@ class ProductController extends Controller
 
                         // Custom sorting for documents
                          // Custom sorting for documents
-                         $desiredOrder = [
-                            'Technical Specification Sheet',
-                            'Warranty Information',
-                            'Horeca Buying Guide',
-                            'Setup & Usage Instructions',
-                            'Product Installation Guide',
-                            'Installation & Elevation Diagram',
-                            'Spare Parts List',
-                            'Product Brochure',
-                        ];
+                        //  $desiredOrder = [
+                        //     'Technical Specification Sheet',
+                        //     'Warranty Information',
+                        //     'Horeca Buying Guide',
+                        //     'Setup & Usage Instructions',
+                        //     'Product Installation Guide',
+                        //     'Installation & Elevation Diagram',
+                        //     'Spare Parts List',
+                        //     'Product Brochure',
+                        // ];
 
-                        $documents = json_decode($product->documents, true);
-                        if (is_array($documents)) {
-                            // Remove .pdf extension from titles
-                            foreach ($documents as &$doc) {
-                                $doc['title'] = preg_replace('/\.pdf$/i', '', $doc['title']);
-                            }
+                        // $documents = json_decode($product->documents, true);
+                        // if (is_array($documents)) {
+                        //     // Remove .pdf extension from titles
+                        //     foreach ($documents as &$doc) {
+                        //         $doc['title'] = preg_replace('/\.pdf$/i', '', $doc['title']);
+                        //     }
 
-                            // Sort documents by desired order
-                            usort($documents, function ($a, $b) use ($desiredOrder) {
-                                $posA = array_search($a['title'], $desiredOrder);
-                                $posB = array_search($b['title'], $desiredOrder);
-                                $posA = $posA === false ? PHP_INT_MAX : $posA;
-                                $posB = $posB === false ? PHP_INT_MAX : $posB;
-                                return $posA <=> $posB;
-                            });
+                        //     // Sort documents by desired order
+                        //     usort($documents, function ($a, $b) use ($desiredOrder) {
+                        //         $posA = array_search($a['title'], $desiredOrder);
+                        //         $posB = array_search($b['title'], $desiredOrder);
+                        //         $posA = $posA === false ? PHP_INT_MAX : $posA;
+                        //         $posB = $posB === false ? PHP_INT_MAX : $posB;
+                        //         return $posA <=> $posB;
+                        //     });
 
-                            $product->documents = $documents;
-                        } else {
-                            $product->documents = [];
-                        }
+                        //     $product->documents = $documents;
+                        // } else {
+                        //     $product->documents = [];
+                        // }
                         
                     
-                        $documents = $product->documents;
+                        // $documents = $product->documents;
 
-                        // If $documents is already an array, skip decoding
-                        if (is_string($documents)) {
-                            $documents = json_decode($documents, true);
-                        }
+                        // // If $documents is already an array, skip decoding
+                        // if (is_string($documents)) {
+                        //     $documents = json_decode($documents, true);
+                        // }
 
-                        // Proceed if it's a valid array
-                        if (is_array($documents)) {
-                            // Remove .pdf extension from titles
-                            foreach ($documents as &$doc) {
-                                if (isset($doc['title'])) {
-                                    $doc['title'] = preg_replace('/\.pdf$/i', '', $doc['title']);
-                                }
-                            }
-                            // Sort documents based on desired order
-                            usort($documents, function ($a, $b) use ($desiredOrder) {
-                                $posA = array_search($a['title'], $desiredOrder);
-                                $posB = array_search($b['title'], $desiredOrder);
-                                $posA = $posA === false ? PHP_INT_MAX : $posA;
-                                $posB = $posB === false ? PHP_INT_MAX : $posB;
-                                return $posA <=> $posB;
-                            });
+                        // // Proceed if it's a valid array
+                        // if (is_array($documents)) {
+                        //     // Remove .pdf extension from titles
+                        //     foreach ($documents as &$doc) {
+                        //         if (isset($doc['title'])) {
+                        //             $doc['title'] = preg_replace('/\.pdf$/i', '', $doc['title']);
+                        //         }
+                        //     }
+                        //     // Sort documents based on desired order
+                        //     usort($documents, function ($a, $b) use ($desiredOrder) {
+                        //         $posA = array_search($a['title'], $desiredOrder);
+                        //         $posB = array_search($b['title'], $desiredOrder);
+                        //         $posA = $posA === false ? PHP_INT_MAX : $posA;
+                        //         $posB = $posB === false ? PHP_INT_MAX : $posB;
+                        //         return $posA <=> $posB;
+                        //     });
 
-                            // Save back to product
-                            $product->documents = $documents;
-                        }
+                        //     // Save back to product
+                        //     $product->documents = $documents;
+                        // }
+                        $desiredOrder = [
+    'Technical Specification Sheet',
+    'Warranty Information',
+    'Horeca Buying Guide',
+    'Setup & Usage Instructions',
+    'Product Installation Guide',
+    'Installation & Elevation Diagram',
+    'Spare Parts List',
+    'Product Brochure',
+];
+
+// Decode documents if it's a string
+$documents = is_string($product->documents)
+    ? json_decode($product->documents, true)
+    : $product->documents;
+
+if (is_array($documents)) {
+    foreach ($documents as &$doc) {
+        // Strip ".pdf" from title
+        if (isset($doc['title'])) {
+            $doc['title'] = preg_replace('/\.pdf$/i', '', $doc['title']);
+        }
+
+        // If 'url' exists, replace it with your custom media URL
+        if (isset($doc['url'])) {
+            $filename = basename($doc['url']);
+            $doc['url'] = url('/media/' . $filename); // example: https://thehorecastore.com/media/filename.pdf
+        }
+    }
+
+    // Sort by desired title order
+    usort($documents, function ($a, $b) use ($desiredOrder) {
+        $posA = isset($a['title']) ? array_search($a['title'], $desiredOrder) : PHP_INT_MAX;
+        $posB = isset($b['title']) ? array_search($b['title'], $desiredOrder) : PHP_INT_MAX;
+        $posA = $posA === false ? PHP_INT_MAX : $posA;
+        $posB = $posB === false ? PHP_INT_MAX : $posB;
+        return $posA <=> $posB;
+    });
+
+    $product->documents = $documents;
+} else {
+    $product->documents = [];
+}
+
 
                         // Handle videos
                         $videoPaths = json_decode($product->video_path, true);
