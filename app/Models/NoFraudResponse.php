01d1a05c994abed8ff1a30e2c3646fd4374e31b4
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-06 18:24:40 +0500

    sd

--- app/Models/NoFraudResponse.php
+++ app/Models/NoFraudResponse.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class NoFraudResponse extends Model
 {
+    protected $table = 'nofraud_responses'; // explicitly defining the table name
+
     protected $fillable = [
         'order_id',
         'response',
+    ];
+
+    protected $casts = [
+        'response' => 'array', // automatically cast JSON response to array
     ];
 }
