598bf2cd48fb5f67e4dac052b75039cb81b45943
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-06 18:04:42 +0500

    sd

--- database/migrations/2025_08_06_182147_add_nofraud_response_to_orders_table.php
+++ database/migrations/2025_08_06_182147_add_nofraud_response_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNofraudResponsesTable extends Migration
+class AddNofraudResponseToOrdersTable  extends Migration
 {
     public function up()
     {
