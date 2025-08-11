0bfaf51f3118a4bf6d8304d99e28d10d9eb7b35d
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-11 23:23:25 +0500

    ds

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -617,7 +617,7 @@ class OrderController extends Controller
 			'orderProducts.product.brand:id,name',
 			'orderProducts.product.currency:id,symbol',
 			'orderProducts.product.sellingUnitAttribute:id,product_id,attribute_value',
-			'payments:id,order_id,transaction_id,payment_mode,amount,status,notes,created_at',
+			'payments:id,order_id,transaction_id,payment_mode,amount,status,notes,created_at,payment_method',
 			'shipments',
 			'creator',
 			'updator',
