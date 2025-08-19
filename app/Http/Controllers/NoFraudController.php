292a67707c690763881f422658a893d844801609
AsharKhursheed <asharkhursheed26@gmail.com>
2025-08-19 15:28:03 +0500

    asa

--- app/Http/Controllers/NoFraudController.php
+++ app/Http/Controllers/NoFraudController.php
@@ -89,10 +89,9 @@ public function screenTransaction(Request $request)
         'billing_state' => 'required|string|max:10',
         'billing_zip' => 'required|string|max:20',
         'billing_country' => 'required|string|size:2',
-        // 'card_last4' => 'required|string|size:4',
-        // 'card_bin' => 'nullable|string|min:6|max:8',
-        'card_last4' => ['required', 'regex:/^\d{4}$/'],
-        'card_bin'   => ['nullable', 'regex:/^\d{6,8}$/'],
+        'card_last4' => 'required|size:4',
+        'card_bin' => 'nullable|min:6|max:8',
+
     ]);
 
     if ($validator->fails()) {
