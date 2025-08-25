e4f2b76626a3d8c5e76bf76cadc5c08f5d4c9f24
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-25 21:14:23 +0500

    cas

--- app/Http/Controllers/FrontEnd/CategoryController.php
+++ app/Http/Controllers/FrontEnd/CategoryController.php
@@ -4252,8 +4252,8 @@ $allChildCategoryIds = $getAllDescendantCategoryIds($category->id);
 $childProductIds = [];
 if (!empty($allChildCategoryIds)) {
     $childProductIds = DB::table('ec_products')
-        ->join('categories', 'ec_products.id', '=', 'categories.product_id')
-        ->whereIn('categories.category_id', $allChildCategoryIds)
+        ->join('categories', 'ec_products.id', '=', 'product_categories.product_id')
+        ->whereIn('product_categories.id', $allChildCategoryIds)
         ->where('ec_products.status', 'published')
         ->pluck('ec_products.id')
         ->toArray();
