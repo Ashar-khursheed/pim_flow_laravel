ab32a7c313c6867bf169419a85c80b2c29371ba8
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-08-27 14:59:04 +0500

    sd

--- app/Http/Controllers/FrontEnd/SliderController.php
+++ app/Http/Controllers/FrontEnd/SliderController.php
@@ -28,7 +28,7 @@ class SliderController extends Controller
      */
     public function index()
     {
-        return response()->json(SimpleSlider::with('items')->get()) ->header('Cache-Control', 'public, max-age=86400');
+        return response()->json(SimpleSlider::with('items')->get());
     }
 
     /**
@@ -56,7 +56,7 @@ class SliderController extends Controller
      */
     public function show($id)
     {
-        $slider = SimpleSlider::with('items')->findOrFail($id) ->header('Cache-Control', 'public, max-age=86400');
+        $slider = SimpleSlider::with('items')->findOrFail($id) ;
         return response()->json($slider);
     }
 
