09e2b5d360a88acfeec7ce25657e9932e46ea232
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-12 00:12:53 +0500

    prerendor

--- app/Http/Middleware/PrerenderMiddleware.php
+++ app/Http/Middleware/PrerenderMiddleware.php
@@ -18,7 +18,7 @@ class PrerenderMiddleware
 
                 // Build full URL for Prerender.io
                 $targetUrl = $request->fullUrl(); // full URL of the current request
-                $prerenderUrl = rtrim(env('PRERENDER_URL'), '/') . '/' . $targetUrl;
+                $prerenderUrl = 'https://service.prerender.io/' . $request->getRequestUri();
 
                 $ch = curl_init($prerenderUrl);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
