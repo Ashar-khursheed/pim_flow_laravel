8134ee4b57d8a771e614a4fb2745e89b31b891d3
brijeshhoreca <webdeveloper07@horecastore.ae>
2025-09-11 23:36:34 +0530

    update product-accessories

--- routes/api.php
+++ routes/api.php
@@ -133,7 +133,6 @@ use App\Http\Controllers\FrontEnd\CompareProductController;
 use App\Http\Controllers\FrontEnd\SitemapController;
 use App\Http\Controllers\FrontEnd\FilterController;
 use App\Http\Controllers\FrontEnd\ShippingReportController;
-use App\Http\Controllers\FrontEnd\FnProductAccessoriesController;
 use App\Http\Middleware\CaptureUtm;
 use App\Models\Lead;
 use App\Models\Utm;
@@ -403,10 +402,7 @@ Route::middleware(['auth:back-end-api', 'user.guard'])->group(function () {
 
 	Route::resource('websites', WebsiteController::class)->only(['index']);
 
-	Route::apiResource('product-accessories', ProductAccessoriesController::class);
-	Route::get('/product-accessories/show/{id}', [ProductAccessoriesController::class, 'show']);	 
-	Route::post('/product-accessories/update/{id}', [ProductAccessoriesController::class, 'update']);
-	Route::delete('/product-accessories/delete/{id}', [ProductAccessoriesController::class, 'destroy']);
+	Route::resource('product-accessories', ProductAccessoriesController::class);	 
 	Route::post('/product-accessories/status/{id}', [ProductAccessoriesController::class, 'updateStatus']);
 	Route::delete('/product-accessories/item/{item_id}', [ProductAccessoriesController::class, 'deleteItem']);
 	
@@ -721,7 +717,6 @@ Route::get('/frontend/products/specification-filters', [F_CategoryController::cl
 Route::get('/frontend/products/specification-filters1', [F_CategoryController::class, 'getSpecificationFilters1']);
 
 Route::post('/frontend/products/filters', [FilterController::class, 'index']);
-Route::get('/frontend/product-accessories', [FnProductAccessoriesController::class, 'index']);
 
 
 Route::get('/frontend/category-with-slug/{slug}', [F_CategoryMenuController::class, 'showCategoryBySlug']);
