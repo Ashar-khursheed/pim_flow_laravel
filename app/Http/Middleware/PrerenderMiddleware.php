75ba9f922c5016b5040fec84f7c06ce96b09a754
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-11 22:52:05 +0500

    sd

---
+++ app/Http/Middleware/PrerenderMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+// app/Http/Middleware/PrerenderMiddleware.php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class PrerenderMiddleware
+{
+    public function handle($request, \Closure $next)
+    {
+        $userAgent = $request->header('User-Agent', '');
+
+        $crawlers = [
+            'googlebot', 'bingbot', 'yahoo', 'baiduspider',
+            'facebookexternalhit', 'twitterbot', 'rogerbot', 'linkedinbot'
+        ];
+
+        foreach ($crawlers as $crawler) {
+            if (stripos($userAgent, $crawler) !== false) {
+                $url = 'https://service.prerender.io/' . ltrim($request->path(), '/');
+                $token = env('PRERENDER_TOKEN');
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-Prerender-Token: $token"]);
+                $html = curl_exec($ch);
+                curl_close($ch);
+                return response($html);
+            }
+        }
+
+        return $next($request);
+    }
+}
