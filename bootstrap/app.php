d975540a35fe37e80146ba25fafb3febb85b33cb
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-12 18:25:48 +0500

    cgsd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsureUserGuard;
 use App\Http\Middleware\EnsureCustomerGuard;
-use App\Http\Middleware\PrerenderMiddleware; // import your custom middleware
 use Illuminate\Auth\AuthenticationException;
 
 
@@ -22,11 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'user.guard' => EnsureUserGuard::class,
             'customer.guard' => EnsureCustomerGuard::class,
-            'prerender' => PrerenderMiddleware::class,
+        
         ]);
 
-        // Prepend so it runs for ALL web routes
-        $middleware->prepend(PrerenderMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (AuthenticationException $e, $request) {
