801be94074e12c26fd3864487c0395745bbe7bdc
brijeshhoreca <webdeveloper07@horecastore.ae>
2025-09-17 20:11:05 +0530

    remove some field

--- app/Http/Controllers/FrontEnd/CcavenueController.php
+++ app/Http/Controllers/FrontEnd/CcavenueController.php
@@ -384,29 +384,7 @@ class CcavenueController extends Controller
             'amount',
             'redirect_url',
             'cancel_url',
-            'language',
-            'billing_name',
-            'billing_address',
-            'billing_city',
-            'billing_state',
-            'billing_zip',
-            'billing_country',
-            'billing_tel',
-            'billing_email',
-            'delivery_name',
-            'delivery_address',
-            'delivery_city',
-            'delivery_state',
-            'delivery_zip',
-            'delivery_country',
-            'delivery_tel',
-            'merchant_param1',
-            'merchant_param2',
-            'merchant_param3',
-            'merchant_param4',
-            'merchant_param5',
-            'promo_code',
-            'customer_identifier'
+            'language'            
         ];
         $merchantId = $this->ccavenueService->getMerchantId();
 
