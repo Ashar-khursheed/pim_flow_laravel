41854948501848144e6602a2ef3a33a81af25d7b
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-09-24 16:17:47 +0530

    Update SupportTicket.php

--- app/Models/FrontEnd/SupportTicket.php
+++ app/Models/FrontEnd/SupportTicket.php
@@ -5,33 +5,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupportTicket extends Model
 {
-    protected $fillable = [
-        'customer_id',
-        'full_name',
-        'email',
-        'company_name',
-        'phone_number',
-        'category_id',
-        'priority_id',
-        'subject',
-        'description',
-        'reference_id',
-        'file_path',
-        'status',
-    ];
+	protected $fillable = [
+		'customer_id',
+		'full_name',
+		'email',
+		'company_name',
+		'phone_number',
+		'category_id',
+		'priority_id',
+		'subject',
+		'description',
+		'reference_id',
+		'file_path',
+		'status',
+	];
 
-    public function customer()
-    {
-        return $this->belongsTo(Customer::class, 'customer_id');
-    }
-    public function category()
-    {
-        return $this->belongsTo(SupportCategory::class, 'category_id');
-    }
+	public function customer()
+	{
+		return $this->belongsTo(Customer::class, 'customer_id');
+	}
 
-    public function priority()
-    {
-        return $this->belongsTo(SupportPriority::class, 'priority_id');
-    }
+	public function category()
+	{
+		return $this->belongsTo(SupportCategory::class, 'category_id');
+	}
 
+	public function priority()
+	{
+		return $this->belongsTo(SupportPriority::class, 'priority_id');
+	}
 }
