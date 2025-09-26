15a326c1fe80603d3615e72b8292487bf00d66ca
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-26 23:28:03 +0500

    chn

--- app/Http/Controllers/FrontEnd/ProductController.php
+++ app/Http/Controllers/FrontEnd/ProductController.php
@@ -93,7 +93,7 @@ class ProductController extends Controller
                 }
 
                 // Start building the base query
-                $query = Product::with(['categories', 'brand' ,'productSuppliers', 'brand.products.reviews' ,  'seoUrl' ])
+                $query = Product::with(['categories', 'brand' ,'productSuppliers', 'brand.products.reviews' ,  'seoUrl' , 'accessories.items'])
                     ->where('status', 'published');
 
 
