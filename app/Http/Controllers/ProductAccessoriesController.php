39add9de249fdce88a9964a8fbccceff84d368a1
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-29 23:03:16 +0500

    sd

--- app/Http/Controllers/ProductAccessoriesController.php
+++ app/Http/Controllers/ProductAccessoriesController.php
@@ -246,7 +246,7 @@ class ProductAccessoriesController extends Controller
             ]);
 
             $accessories = collect($request->accessories)->map(function ($item) {
-                $item['name'] = addslashes($item['name']);
+                // $item['name'] = addslashes($item['name']);
                 return $item;
             })->toArray();
 
