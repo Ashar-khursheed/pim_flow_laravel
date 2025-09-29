3c840df65fb25b9dd2436abfa8ea1118213523a7
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-29 22:14:09 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -907,11 +907,10 @@ Route::get('/frontend/search-logs', [F_SearchLogController::class, 'index']);
 Route::get('/frontend/menu-banners', [F_MenuBannerController::class, 'index']);
 Route::get('/frontend/menu-banners/{id}', [F_MenuBannerController::class, 'show']);
 Route::get('/frontend/menu-banners/category/{category_id}', [F_MenuBannerController::class, 'showCategory']);
+Route::post('/frontend/auth/Stax', [F_StaxPaymentController::class, 'checkout']);
 
+ 
 	// Route::get('/redirects/from/{from}', [RedirectLinkController::class, 'getByFrom']);
 	Route::get('redirects/from/{from}', [RedirectLinkController::class, 'getByFrom'])
      ->where('from', '.*');
 
-	Route::post('/frontend/auth/Stax', [F_StaxPaymentController::class, 'checkout']);
-
- 
