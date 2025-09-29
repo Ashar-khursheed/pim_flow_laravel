2665158a1e223846b2e58d49808b0f9d3f409491
Ashar-Khursheed <asharkhursheed26@gmail.com>
2025-09-29 23:05:39 +0500

    productac

--- app/Http/Controllers/ProductAccessoriesController.php
+++ app/Http/Controllers/ProductAccessoriesController.php
@@ -483,7 +483,7 @@ class ProductAccessoriesController extends Controller
             // Refresh accessory items (delete old and insert new)
             $accessory->items()->delete();
             $accessories = collect($request->accessories)->map(function ($item) {
-                $item['name'] = addslashes($item['name']);
+                $item['name'] = trim($item['name'], '"'); // remove surrounding quotes
                 return $item;
             })->toArray();
             $accessory->items()->createMany($accessories);
