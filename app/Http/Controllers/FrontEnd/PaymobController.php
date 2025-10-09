a354fd985f6063c4d0390705aa76e31a92f86759
brijeshhoreca <webdeveloper07@horecastore.ae>
2025-10-09 23:47:42 +0530

    remove url

--- app/Http/Controllers/FrontEnd/PaymobController.php
+++ app/Http/Controllers/FrontEnd/PaymobController.php
@@ -497,7 +497,7 @@ class PaymobController extends Controller
                 'billing_data' => $billingData,
                 'currency' => 'AED', // Fixed: Should be AED for UAE, not EGP
                 'integration_id' => env('PAYMOB_LINK_ID'),
-                'redirect_url' => 'https://www.uae.thehorecastore.co/thanks',
+                // 'redirect_url' => 'https://www.uae.thehorecastore.co/thanks',
                 'notification_url' => 'https://testpim.thehorecastore.co/api/paymob/webhook',                 
             ]);
             $paymentToken = $paymentKeyResponse->json()['token'];
