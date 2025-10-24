2363955dc7416552c73d748c8758b39d4dbed542
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-10-24 15:55:34 +0530

    Update constant.php

--- app/Helpers/constant.php
+++ app/Helpers/constant.php
@@ -677,7 +677,6 @@ if (!function_exists('order_cc_mails')) {
 			'css01@horecastore.ae',
 			'cbfb@horecastore.ae',
 			'procurement@horecastore.ae',
-			'webdeveloper04@horecastore.ae',
 		];
 
 		$testMails = [
