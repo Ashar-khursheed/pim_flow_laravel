c76dbf48b827b8be7b4650026e48f05dd4687418
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-11-07 21:28:27 +0530

    Create 2025_11_07_174918_create_category_translations_table.php

---
+++ database/migrations/2025_11_07_174918_create_category_translations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::dropIfExists('category_translations');
+
+		Schema::create('category_translations', function (Blueprint $table) {
+			$table->id();
+			$table->string("locale", 2);
+			$table->integer("category_id");
+			$table->text("name_tr");
+		});
+
+		if (in_array(config('app.website'), ['UAE', 'UAE_T', 'SA'])) {
+			$records = DB::table('categories')->select('id', 'name')->get();
+			foreach ($records as $record) {
+				DB::table('category_translations')->insert([
+					'locale' => 'en',
+					'category_id' => $record->id,
+					'name_tr' => $record->name,
+				]);
+			}
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('category_translations');
+	}
+};
