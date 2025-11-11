c465c4c9168dccf767f2eecd05e9b353979827e5
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-11-11 18:28:54 +0500

    sd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2852,7 +2852,8 @@ class ProductController extends BaseController
  *             @OA\Property(property="status", type="string", example="error"),
  *             @OA\Property(property="message", type="string", example="Product not found.")
  *         )
- *     )
+ *     ),
+ *  security={{"bearerAuth":{}}}
  * )
  */
 public function getStoreUrl(Request $request)
