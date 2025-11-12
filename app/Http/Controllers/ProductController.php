ca6f9f74acf4d36f1a93f0a62f5f3199868f37bd
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-11-12 20:16:42 +0500

    sd

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,6 +45,8 @@ class ProductController extends BaseController
 	 *     ),
 	 *     @OA\Parameter(name="from_date", in="query", @OA\Schema(type="string", format="date")),
 	 *     @OA\Parameter(name="to_date", in="query", @OA\Schema(type="string", format="date")),
+	 *     @OA\Parameter(name="updated_from_date", in="query", @OA\Schema(type="string", format="date")),
+	 *     @OA\Parameter(name="updated_to_date", in="query", @OA\Schema(type="string", format="date")),
 	 *     @OA\Parameter(
 	 *         name="per_page",
 	 *         in="query",
@@ -127,6 +129,21 @@ class ProductController extends BaseController
 			$to = $request->to_date . ' 23:59:59';
 
 			$records = Product::whereBetween('created_at', [$from, $to])->where('status', 'published')->pluck('id');
+			return response()->json([
+				'success' => true,
+				'message' => __('msg_rec_list'),
+				'data' => $records,
+			]);
+		}
+
+		  if ($request->filled('updated_from_date') && $request->filled('updated_to_date')) {
+			$from = $request->updated_from_date . ' 00:00:00';
+			$to = $request->updated_to_date . ' 23:59:59';
+
+			$records = Product::whereBetween('updated_at', [$from, $to])
+				->where('status', 'published')
+				->pluck('id');
+
 			return response()->json([
 				'success' => true,
 				'message' => __('msg_rec_list'),
