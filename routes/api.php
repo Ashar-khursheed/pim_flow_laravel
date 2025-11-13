87d4c61b257d15af878679f17db84cf9ad58e2e1
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-11-13 16:50:46 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -505,7 +505,10 @@ Route::middleware(['auth:back-end-api', 'user.guard'])->group(function () {
 	Route::apiResource('roles', RoleController::class);
 
 
-	Route::apiResource('reviews', ReviewController::class);
+		Route::apiResource('reviews', ReviewController::class);
+	Route::post('reviews/import', [ReviewController::class,'import']);
+	Route::post('reviews/export', [ReviewController::class,'export']);
+	Route::post('reviews/exportReview', [ReviewController::class,'exportReview']);
 	Route::apiResource('sliders', SliderController::class);
 
 	// Discount API Routes
