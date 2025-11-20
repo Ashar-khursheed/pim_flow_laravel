f6cfda9313d6c54aff095608611917c47447d263
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-11-20 16:48:18 +0500

    ea

--- app/Models/SeoSecondaryKeyword.php
+++ app/Models/SeoSecondaryKeyword.php
@@ -18,15 +18,5 @@ class SeoSecondaryKeyword extends Model
 	{
 		return $this->belongsTo(SeoManagement::class, 'primary_keyword_id');
 	}
-	protected static function boot()
-{
-    parent::boot();
-
-    static::creating(function ($model) {
-        if (empty($model->secondary_keyword)) {
-            $model->secondary_keyword = '';
-        }
-    });
-}
-
+	
 }
