06d35bf79edd652035a45dc887784d8274b1265e
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-11-27 00:31:01 +0500

    sd

--- app/Http/Controllers/FrontEnd/FinanceController.php
+++ app/Http/Controllers/FrontEnd/FinanceController.php
@@ -89,7 +89,7 @@ class FinanceController extends Controller
         }
 
         $data = $validator->validated();
-        $data['customer_id'] = Auth::id() ?? 1;
+        $data['customer_id'] = Auth::id() ;
         $data['created_by'] = '0';
         $data['updated_by'] = '0';
 
