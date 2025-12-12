86c4135d8abe39c0e93cad7d07c170d83fbdb725
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-12-12 16:34:26 +0530

    c

--- app/Jobs/NetTerm/NetTermMailJob.php
+++ app/Jobs/NetTerm/NetTermMailJob.php
@@ -41,8 +41,8 @@ class NetTermMailJob implements ShouldQueue
 
 		if (!empty($finance)) {
 			$fromEmail = match (config('app.website')) {
-				'US'  => 'support@thehorecastore.com',
-				'UAE'  => 'support@horecastore.ae',
+				'US'  => 'sales@thehorecastore.com',
+				'UAE'  => 'hello@horecastore.ae',
 				'US_T' => 'test_us_support@thehorecastore.co',
 				'UAE_T' => 'test_uae_support@thehorecastore.co',
 				default => 'test_support@thehorecastore.co',
