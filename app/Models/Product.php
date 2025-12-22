55b888d765685454e9e760e54488d53cc9afad75
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2025-12-22 19:04:03 +0530

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -51,12 +51,12 @@ class Product extends Model implements TranslatableContract
 {
 	use Translatable;
 
-	// public $translatedAttributes = [
-	// 	'name_tr',
-	// 	'description_tr',
-	// 	'benefits_features_tr',
-	// 	'images_tr',
-	// ];
+	public $translatedAttributes = [
+		'name_tr',
+		'description_tr',
+		'benefits_features_tr',
+		'images_tr',
+	];
 
 	public $translatedAttributes = [];
 
