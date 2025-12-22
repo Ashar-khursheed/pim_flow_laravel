9cba7c3209d59041118a14d0598aa58b9dcf3260
Ashar-khursheed <asharkhursheed26@gmail.com>
2025-12-22 20:34:37 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -660,7 +660,7 @@ Route::middleware(['auth:back-end-api', 'user.guard'])->group(function () {
 	Route::post('/nofraud/process/{order_id}', [NoFraudController::class, 'processNoFraud']);
 
 	Route::apiResource('/get-in-touch', GetInTouchController::class);
-	Route::get('/get-in-touch/{id}', [GetInTouchController::class, 'show']);
+	Route::get('frontend/get-in-touch/{id}', [GetInTouchController::class, 'show']);
 
 
 
