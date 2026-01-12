24c83774a20dd5ffd7fcfec8b3fe9710e4689556
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2026-01-12 18:53:03 +0530

    c

--- resources/views/emails/quotes/quote-placed.blade.php
+++ resources/views/emails/quotes/quote-placed.blade.php
@@ -6,131 +6,68 @@
 	<title>Quotation Ready - HorecaStore</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<style>
-		@media only screen and (max-width: 580px) {
-			.wrapper {
+		@media only screen and (max-width: 600px) {
+			.container {
 				width: 100% !important;
 				padding: 20px !important;
+			}
+
+			.order-button {
+				display: block;
+				margin: 15px 0;
 			}
 		}
 	</style>
 </head>
-
-<body style="margin:0; padding:0; background:#ffffff; font-family:'Noto Sans', sans-serif; color:black;">
-	<!-- Preheader text -->
+@php
+	use Illuminate\Support\Str;
+@endphp
+<body style="margin: 0; padding: 0; background: #ffffff; font-family: 'Noto Sans', sans-serif; color: black;">
+	<!-- Preheader text: hidden but visible in email previews -->
 	<span style="display: none; font-size: 1px; color: #ffffff; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
 		Your HorecaStore quotation is ready to download.
 	</span>
 
-	<table role="presentation" width="650" cellspacing="0" cellpadding="0" border="0" style="background:#ffffff; margin: auto;">
+	<table width="100%" cellspacing="0" cellpadding="0" border="0" style="background:#f8f8f8; font-family: 'Noto Sans', sans-serif;">
 		<tr>
 			<td align="center">
-				<table class="wrapper" width="650" cellpadding="0" cellspacing="0" border="0" style="border:1px solid #eaeaea; padding:20px; font-family:'Noto Sans', sans-serif;  background:#ffffff; ">
+				<table class="container" width="650" cellspacing="0" cellpadding="10" border="0" style="background:#ffffff; border:1px solid #eaeaea; font-family: 'Noto Sans', sans-serif;">
 					<tr>
-						<td style="padding-bottom: 20px;">
-							<img src="{{ $logoUrl }}" alt="Logo" width="120" style="font-family:'Noto Sans', sans-serif;">
-						</td>
-					</tr>
-					<tr>
-						<td style="font-size: 16px; font-weight: 500; line-height: 24px; font-family:'Noto Sans', sans-serif; color: black;">
-							Hi <span style="color:#26683A; font-weight: 500;">{{ $name }}</span>!
+						<td align="left">
+							<img src="{{ $logoUrl }}" alt="Logo" width="120">
 						</td>
 					</tr>
 
-					<tr>
-						<td style="font-size: 14px; line-height: 25px; font-family:'Noto Sans', sans-serif; color: black;">
-							Thank you for generating a quotation from HorecaStore — the smart way to source for restaurants, hotels, and more.
-						</td>
-					</tr>
-
-					<tr>
-						<td style="padding: 20px 0 10px; font-size: 16px; font-family:'Noto Sans', sans-serif; color: black; line-height: 24px;">
-							<span style="font-weight: 500; font-size: 16px; font-family:'Noto Sans', sans-serif; color: black; line-height: 24px;">
-								But this isn’t just another quote. It’s backed by our Dual Promise:
-							</span>
-						</td>
-					</tr>
-
-					<tr>
-						<td style="font-size: 14px; line-height: 25px; font-family:'Noto Sans', sans-serif; color: black;">
-							<img src="{{ $rightPngURL }}" width="18" style="vertical-align: middle; width: 24px; height: 24px;">
-							<span style="font-weight: 500; font-size: 14px; line-height: 25px; font-family:'Noto Sans', sans-serif; color: black;">
-								Unbeatable Pricing
-							</span><br>
-							<img src="{{ $rightPngURL }}" width="18" style="vertical-align: middle; width: 24px; height: 24px;">
-							<span style="font-weight: 500; font-size: 14px; line-height: 25px; font-family:'Noto Sans', sans-serif; color: black;">
-								Stress-Free Sourcing
-							</span>
-						</td>
-					</tr>
-					<tr>
-						<td style="padding-top: 15px; font-size: 14px; line-height: 20px; font-family:'Noto Sans', sans-serif; color: black;">
-							At HorecaStore, we know procurement is more than just comparing numbers. That’s why over 5,000+ restaurants, hotels, and catering businesses trust us — not just for products, but for peace of mind.
-						</td>
-					</tr>
-
-					<tr>
-						<td style="padding-top: 15px; padding-bottom: 5px;   font-family:'Noto Sans', sans-serif; font-size: 14px; color: black; line-height: 24px;">
-							<span style="font-weight: 600; font-family:'Noto Sans', sans-serif; font-size: 15px; color: black; line-height: 24px;">
-								Beyond the Quote: The HorecaStore Advantage
-							</span>
-						</td>
-					</tr>
 					<tr>
 						<td>
-							<table role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0" style="font-family: 'Noto Sans', sans-serif;">
+							<p style="font-size:16px; line-height:25px; font-weight: 500; font-family: 'Noto Sans', sans-serif; margin: 0;">
+								Hi
+								<strong style="color:#26683A; font-family: 'Noto Sans', sans-serif; font-size:16px; line-height:25px; margin: 0;">
+									{{ $name }}
+								</strong>!
+							</p>
+							<p style="font-size:14px; line-height:25px; font-weight: 500; font-family: 'Noto Sans', sans-serif; margin: 0;">
+								Thank you for generating a quotation from HorecaStore — the smart way to source for restaurants, hotels, and more.
+							</p>
+						</td>
+					</tr>
+
+					<tr>
+						<td style="padding-top:10px; font-family: 'Noto Sans', sans-serif;">
+							<p style="font-weight: 500; font-family: 'Noto Sans', sans-serif; font-size:15px; line-height:22px; padding: 0; margin: 8px 0;">
+								But this isn't just another quote. It's backed by our Dual Promise:
+							</p>
+							<table cellpadding="0" cellspacing="0" border="0" width="100%" style="font-family: 'Noto Sans', sans-serif; margin-top: 5px;">
 								<tr>
-									<td style="width: 0%; font-family: 'Noto Sans', sans-serif;  max-width: 24px; width: 24px; height: 24px;">
-										<img src="{{ $rightPngURL }}" alt="" width="24" height="24" style="max-width: 24px; width: 24px; height: 24px;">
-									</td>
-									<td valign="top" style="padding-left: 5px; font-family: 'Noto Sans', sans-serif;font-size:14px;line-height:25px;">
-										<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-											Warranty Included
-										</strong>
-										– Every product, No hidden term
+									<td valign="top" style="padding-bottom: 8px; font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="color:#232425; margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px; font-weight: 500;">Unbeatable Pricing</span>
 									</td>
 								</tr>
 								<tr>
-									<td style="width: 0%; font-family: 'Noto Sans', sans-serif; max-width: 24px; width: 24px; height: 24px;">
-										<img src="{{ $rightPngURL }}" alt="" width="24" height="24"  style="max-width: 24px; width: 24px; height: 24px;">
-									</td>
-									<td valign="top" style="padding-left: 5px; font-family: 'Noto Sans', sans-serif;font-size:14px;line-height:25px;">
-										<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-											Free Shipping, Delivery & Installation
-										</strong>
-										– No lifting, No hassle
-									</td>
-								</tr>
-								<tr>
-									<td style=" width: 0%; font-family: 'Noto Sans', sans-serif; max-width: 24px; width: 24px; height: 24px;">
-										<img src="{{ $rightPngURL }}" alt="" width="24" height="24"  style="max-width: 24px; width: 24px; height: 24px;">
-									</td>
-									<td valign="top" style="padding-left: 5px;font-family: 'Noto Sans', sans-serif;font-size:14px;line-height:25px;">
-										<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-											Returns & Refunds
-										</strong>
-										— Up to 90 Days – Full flexibility, Zero stress
-									</td>
-								</tr>
-								<tr>
-									<td style=" width: 0%; font-family: 'Noto Sans', sans-serif; max-width: 24px; width: 24px; height: 24px;">
-										<img src="{{ $rightPngURL }}" alt="" width="24" height="24"  style="max-width: 24px; width: 24px; height: 24px;">
-									</td>
-									<td valign="top" style="padding-left: 5px;font-family: 'Noto Sans', sans-serif;font-size:14px;line-height:25px;">
-										<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-											Sourcing, Simplified
-										</strong>
-										– One Supplier. One Invoice.
-									</td>
-								</tr>
-								<tr>
-									<td style=" width: 0%; font-family: 'Noto Sans', sans-serif; max-width: 24px; width: 24px; height: 24px;">
-										<img src="{{ $rightPngURL }}" alt="" width="24" height="24"  style="max-width: 24px; width: 24px; height: 24px;">
-									</td>
-									<td valign="top" style="padding-left: 5px;font-family: 'Noto Sans', sans-serif;font-size:14px;line-height:25px;">
-										<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-											24/7 Customer Support
-										</strong>
-										– Because service truly matters
+									<td valign="top" style="font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="color:#232425; margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px; font-weight: 500;">Stress-Free Sourcing</span>
 									</td>
 								</tr>
 							</table>
@@ -138,55 +75,110 @@
 					</tr>
 
 					<tr>
-						<td valign="top" style="font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height: 25px; display: flex; align-items: center; margin-top: 10px; margin-bottom: 5px;">
-							{{-- <img src="{{ $mailIconURL }}" alt="" width="24" height="24" style="margin-right: 5px; max-width: 24px; width: 24px; height: 24px;"> --}}
-							<strong style="color:black; font-weight:600; font-family: 'Noto Sans', sans-serif;">
-								Your Quotation is Ready
-							</strong>
+						<td>
+							<p style="font-size:14px;line-height: 22px;font-family: 'Noto Sans', sans-serif;padding: 0;margin: 8px 0;">
+								At HorecaStore, we know procurement is more than just comparing numbers. That's why over 5,000+ restaurants, hotels, and catering businesses trust us — not just for products, but for peace of mind.
+							</p>
 						</td>
 					</tr>
 
 					<tr>
-						<td style="font-family:'Noto Sans', sans-serif;">
-							<a href="{{ $downloadLink }}" style="background-color:#26683A; color:#ffffff; padding:12px 24px; text-decoration:none; font-size:14px; border-radius:5px; display:inline-block; font-family:'Noto Sans', sans-serif;">
+						<td style="padding-top:10px; font-family: 'Noto Sans', sans-serif;">
+							<p style="font-weight: 600; font-family: 'Noto Sans', sans-serif; font-size:15px; line-height:22px; padding: 0; margin: 8px 0;">
+								Beyond the Quote: The HorecaStore Advantage
+							</p>
+							<table cellpadding="0" cellspacing="0" border="0" width="100%" style="font-family: 'Noto Sans', sans-serif; margin-top: 5px;">
+								<tr>
+									<td valign="top" style="padding-bottom: 8px; font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px;"><strong style="font-weight:600; font-family: 'Noto Sans', sans-serif;">Warranty Included</strong> – Every product, No hidden term</span>
+									</td>
+								</tr>
+								<tr>
+									<td valign="top" style="padding-bottom: 8px; font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px;"><strong style="font-weight:600; font-family: 'Noto Sans', sans-serif;">Free Shipping, Delivery & Installation</strong> – No lifting, No hassle</span>
+									</td>
+								</tr>
+								<tr>
+									<td valign="top" style="padding-bottom: 8px; font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px;"><strong style="font-weight:600; font-family: 'Noto Sans', sans-serif;">Returns & Refunds</strong> — Up to 90 Days – Full flexibility, Zero stress</span>
+									</td>
+								</tr>
+								<tr>
+									<td valign="top" style="padding-bottom: 8px; font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px;"><strong style="font-weight:600; font-family: 'Noto Sans', sans-serif;">Sourcing, Simplified</strong> – One Supplier. One Invoice.</span>
+									</td>
+								</tr>
+								<tr>
+									<td valign="top" style="font-family: 'Noto Sans', sans-serif;">
+										<img src="{{ $rightPngURL }}" alt="right" width="20" height="20" style="vertical-align: middle;" />
+										<span style="margin-left: 5px; font-family: 'Noto Sans', sans-serif; font-size: 14px; line-height:20px;"><strong style="font-weight:600; font-family: 'Noto Sans', sans-serif;">24/7 Customer Support</strong> – Because service truly matters</span>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+
+					<tr>
+						<td style="padding-top:10px; font-family: 'Noto Sans', sans-serif;">
+							<p style="font-weight: 600; font-family: 'Noto Sans', sans-serif; font-size:15px; line-height:22px; padding: 0; margin: 8px 0;">
+								Your Quotation is Ready
+							</p>
+						</td>
+					</tr>
+
+					<tr>
+						<td>
+							<a href="{{ $downloadLink }}" class="order-button" style="background:#26683A; color:#fff; padding:12px 24px; margin-top: 10px; font-size:14px; line-height:20px; text-decoration:none; border-radius:5px; display:inline-block; font-family: 'Noto Sans', sans-serif;">
 								Download Now
 							</a>
 						</td>
 					</tr>
 
 					<tr>
-						<td style="font-size: 14px; padding-top: 20px; line-height: 24px; font-family:'Noto Sans', sans-serif; color: black;">
-							Ready to place your order? Click Below:
-							<br>
-							<a href="{{ $orderLink }}" style="color:#26683A; font-weight: 600; text-decoration: underline; font-family:'Noto Sans', sans-serif;">Place Your Order
-							</a>
+						<td>
+							<p style="font-size:14px;line-height: 22px;font-family: 'Noto Sans', sans-serif;padding: 0;margin: 8px 0;">
+								Ready to place your order?
+								<a href="{{ $orderLink }}" style="color:#26683A; font-weight: 600; text-decoration: underline; font-family: 'Noto Sans', sans-serif;">Place Your Order</a>
+							</p>
 						</td>
 					</tr>
 
 					<tr>
-						<td style="font-size: 14px; padding-top: 15px; font-family:'Noto Sans', sans-serif; color: black; font-weight: 600;">
-							Behind Every Great Plate, There’s HorecaStore
-						</td>
-					</tr>
-					<tr>
-						<td style="font-size: 14px; padding: 10px 0; font-family:'Noto Sans', sans-serif; color: black;">
-							Warm regards,<br>
-							<p style="font-weight: 500; margin: 5px 0; color:#26683A; font-family:'Noto Sans', sans-serif;">– Team HorecaStore</p>
+						<td>
+							<table width="100%" cellspacing="0" cellpadding="0" border="0">
+								<tr>
+									<td style="font-size:14px; border-top:3px solid #E2E8F0; padding-top:15px; padding-bottom:5px;  font-family: 'Noto Sans', sans-serif">
+										Behind Every Great Plate, There's HorecaStore
+									</td>
+								</tr>
+								<tr>
+									<td style="font-weight: 500; line-height: 24px; margin: 0; padding: 0; font-family: 'Noto Sans', sans-serif; font-size:14px;">
+										Thank you for choosing HorecaStore - where your business gets the best, for less.
+									</td>
+								</tr>
+								<tr>
+									<td style="font-size:14px; color: #26683A; font-weight: 500; line-height:22px; font-family: 'Noto Sans', sans-serif">
+										<p style="padding: 5px 0; color: #26683A; font-weight: 500; font-size: 14px; margin: 0; line-height: 20px; font-family: 'Noto Sans', sans-serif;">
+											&ndash; Team HorecaStore
+										</p>
+									</td>
+								</tr>
+							</table>
 						</td>
 					</tr>
 				</table>
 
-				<table width="650" cellspacing="0" cellpadding="0" border="0" style="border-top:2px solid #E2E8F0; background-color:rgba(226,232,240,0.3); padding:20px; font-size:12px; color:#3F3F3F; line-height:18px; font-family:'Noto Sans', sans-serif;">
+				<table width="650" cellspacing="0" cellpadding="0" border="0" style="padding:10px; border-top:3px solid #E2E8F0; background-color: rgba(226, 232, 240, 0.3); font-size:11px; color:#3F3F3F;">
 					<tr>
 						<td>
-							<p style="margin:5px 0; font-weight:500; font-size:14px; font-family:'Noto Sans', sans-serif;">MORE INFORMATION</p>
-							<p style="margin: 5px 0;;font-size:12px; font-family: 'Noto Sans', sans-serif;">
-								We strive to respond to all quotation requests promptly. However, in rare cases, delays may occur. We appreciate your understanding.
-							</p>
-							<p style="margin: 5px 0;;font-size:12px; font-family: 'Noto Sans', sans-serif;">
+							<p style="margin: 0;font-size:12px; font-family: 'Noto Sans', sans-serif;">
 								©{{ now()->year }} {{ $siteUrl }}. All rights reserved. HorecaStore, TheHorecaStore.com, and the HorecaStore.ae logo are trademarks of HorecaStore LLC or its affiliates.
 							</p>
-							<p style="margin: 5px 0;;font-size:12px; font-family: 'Noto Sans', sans-serif;">
+							<p style="margin: 8px 0 0; font-size:12px; font-family: 'Noto Sans', sans-serif;">
 								For support or inquiries, contact us at
 								<a href="mailto:{{ $siteEmail }}" style="color:#186737; font-family: 'Noto Sans', sans-serif; font-size:12px; line-height:18px;">
 									{{ $siteEmail }}
@@ -199,5 +191,4 @@
 		</tr>
 	</table>
 </body>
-
 </html>
