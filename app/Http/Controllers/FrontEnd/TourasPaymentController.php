d1e8c33dc794c02db4d2ded6382a88eee5c95d0d
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2026-02-04 18:20:10 +0530

    Update TourasPaymentController.php

--- app/Http/Controllers/FrontEnd/TourasPaymentController.php
+++ app/Http/Controllers/FrontEnd/TourasPaymentController.php
@@ -30,7 +30,7 @@ class TourasPaymentController extends Controller
 		// $this->successUrl = 'http://pim.devs/api/frontend/touras/callback';
 		// $this->failureUrl = 'http://pim.devs/api/frontend/touras/callback';
 		// $this->frontendUrl = config('app.url');
-		$this->frontendUrl = 'http://localhost:3000/touras-success';
+		$this->frontendUrl = 'http://localhost:3000';
 	}
 
 	/**
