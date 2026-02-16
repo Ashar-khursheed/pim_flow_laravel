851061f7472010a5d32230b3e39145424892797c
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-02-16 17:09:57 +0500

    sd

--- app/Http/Controllers/ProductSupplierController.php
+++ app/Http/Controllers/ProductSupplierController.php
@@ -59,6 +59,7 @@ class ProductSupplierController extends BaseController
 			'product_suppliers.*',
 			'ec_products.name as product_name',
 			 'ec_products.sku as product_sku',
+			'ec_products.status as product_status',
 			'vendors.name as vendor_name'
 		);
 
