f1fcaf3bdb9213d196e776c166cac3bf7c38bbe4
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-02-17 17:12:03 +0500

    sd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -410,7 +410,7 @@ class OrderController extends Controller
 			'coupon_id' => 'nullable|integer',
 			'discount' => 'nullable|numeric|min:0',
 
-			'payment_mode' => 'nullable|in:Touras,CCAvenue,Stripe,Square,Check Payment,Ascentium Financing,Approve Financing,Resolve Financing,Net Terms',
+			'payment_mode' => 'nullable|in:ADCB Touras,CCAvenue,Stripe,Square,Check Payment,Ascentium Financing,Approve Financing,Resolve Financing,Net Terms',
 			'pay_with_cheque' => 'nullable|boolean',
 			'cheque_img' => 'nullable|required_if:pay_with_cheque,true|file|mimes:jpeg,jpg,png,webp|max:5120',
 			'cheque_img_back' => 'nullable|required_if:pay_with_cheque,true|file|mimes:jpeg,jpg,png,webp|max:5120',
@@ -1408,7 +1408,7 @@ class OrderController extends Controller
 			'coupon_id' => 'nullable|integer',
 			'discount' => 'nullable|numeric|min:0',
 
-			'payment_mode' => 'nullable|in:Touras,CCAvenue,Stripe,Check Payment,Ascentium Financing,Approve Financing,Resolve Financing,Net Terms',
+			'payment_mode' => 'nullable|in:ADCB Touras,CCAvenue,Stripe,Check Payment,Ascentium Financing,Approve Financing,Resolve Financing,Net Terms',
 			'pay_with_cheque' => 'nullable|boolean',
 			'cheque_img' => 'nullable|file|mimes:jpeg,jpg,png,webp|max:5120',
 			'cheque_img_back' => 'nullable|file|mimes:jpeg,jpg,png,webp|max:5120',
