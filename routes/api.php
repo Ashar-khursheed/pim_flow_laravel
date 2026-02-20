e7613e056f2d6aa7c05fbd8827dbec38dec0aafd
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-02-20 22:22:54 +0500

    sd

--- routes/api.php
+++ routes/api.php
@@ -1147,4 +1147,4 @@ Route::get('frontend/touras/pay/{order}', [F_TourasPaymentController::class, 'pa
 Route::any('frontend/touras/callback', [F_TourasPaymentController::class, 'handleCallback'])->name('frontend.touras.callback');
 
 Route::apiResource('frontend/horeca-pages',  F_HorecaPageController::class);
-Route::get('frontend/horeca-pages-by-slug/{slug}', [F_HorecaPageController::class, 'showBySlug']);
+Route::get('frontend/horeca-pages-by-slug/{slug}', [F_HorecaPageController::class, 'showBySlug'])->where('slug', '.*');
