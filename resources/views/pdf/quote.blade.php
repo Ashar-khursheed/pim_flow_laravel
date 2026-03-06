914f1f46bc0fb46174515fe7d9611aa9c24b21c3
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2026-03-07 00:05:08 +0530

    Update quote.blade.php

--- resources/views/pdf/quote.blade.php
+++ resources/views/pdf/quote.blade.php
@@ -213,7 +213,7 @@ $pageNumber = 1;
 			@endforeach
 
 
-			@if(($index === 0 && in_array(count($chunk), [4, 5])) || ($index > 0 && count($chunk) == 6) || ($index != 0 && $index === count($chunks)-1 && in_array(count($chunk), [5, 6])))
+			@if(($index === 0 && in_array(count($chunk), [3, 4, 5])) || ($index > 0 && count($chunk) == 6) || ($index != 0 && $index === count($chunks)-1 && in_array(count($chunk), [5, 6])))
 			@if(($index === 0 && count($chunk) == 4) || ($index === count($chunks)-1 && in_array(count($chunk), [5])))
 			<div style="page-break-before: always;"></div>
 			@endif
@@ -406,7 +406,7 @@ $pageNumber = 1;
 				</tr>
 			</table>
 
-			@if(($index === 0 && in_array(count($chunk), [1, 2, 3])) || ($index != 0 && $index === count($chunks)-1 && in_array(count($chunk), [2, 3, 4])))
+			@if(($index === 0 && in_array(count($chunk), [1, 2])) || ($index != 0 && $index === count($chunks)-1 && in_array(count($chunk), [2, 3, 4])))
 			<table style="width: 100%; border-top: 1px solid black; margin-top: 10px; padding-top: 2px; font-size: 12px; font-family: 'Inter', sans-serif; position: absolute; bottom: 70px;">
 				<tr>
 					<td style="text-align: left;">
