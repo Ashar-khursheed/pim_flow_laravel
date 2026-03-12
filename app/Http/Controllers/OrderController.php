b9cd383d623c223f277a8455e22c8f7b7ad2db27
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2026-03-12 22:48:56 +0530

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -662,7 +662,6 @@ class OrderController extends Controller
 			return response()->json([
 				'success' => true,
 				'message' => 'Order created successfully',
-				'data' => $order
 			], 201);
 
 		} catch (\Exception $e) {
@@ -1593,7 +1592,6 @@ class OrderController extends Controller
 			return response()->json([
 				'success' => true,
 				'message' => 'Order updated successfully',
-				'data' => $order
 			], 200);
 
 		} catch (\Exception $e) {
