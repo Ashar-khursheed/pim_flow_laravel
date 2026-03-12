c3215f7365ae2752d504f22c3d373c369cf3666a
TanujKumarBhardwaj <webdeveloper04@horecastore.ae>
2026-03-12 20:43:15 +0530

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -452,7 +452,7 @@ class OrderController extends Controller
 
 		$margin = $address->country->margin ?? 0;
 
-		dd($margin);
+		dd($address->toArray(), $margin);
 
 		$customer = Customer::select('is_tax_free')->find($request->customer_id);
 		$amountCalculations = $this->calculateAmount($request, $customer->is_tax_free, margin: $margin);
